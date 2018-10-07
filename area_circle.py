86240d11b6b341497b9a8e9e32a7cb68548ce8d4
abdulwasih <vasih95@gmail.com>
2018-10-07 21:41:53 +0530

    first commit

---
+++ area_circle.py
@@ -0,0 +1,17 @@
+#Circle Area Program
+
+#importing math library
+import math
+
+#inputting radius of the circle
+#type conversion from string to float 
+radius = float(input("Enter the radius of the circle:\t"))
+
+#area of circle= 4*pi*r*r
+area = (math.pi)*(radius**2)  #radius**2  = radius*radius
+
+#printing area of the circle
+print("Area of the circle is: ",area)
+
+
+
