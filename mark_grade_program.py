1098376abd90d91def324bcf5e4fb0891fba7286
abdulwasih <vasih95@gmail.com>
2018-10-07 23:00:46 +0530

    first commit

---
+++ mark_grade_program.py
@@ -0,0 +1,9 @@
+mark = int(input("Enter your mark:\t"))
+if mark>=95:
+    print("A+")
+elif mark>=90 and mark<95:
+    print("A")
+elif mark>=85 and mark<90:
+    print("B+")
+elif mark>=80 and mark<85:
+    print("B")
