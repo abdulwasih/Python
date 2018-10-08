5790dbdaf0893e8759c20f49972ba2ca550921df
abdulwasih <vasih95@gmail.com>
2018-10-08 07:31:30 +0530

    first commit

---
+++ substring_count.py
@@ -0,0 +1,30 @@
+
+
+string1="aaabbabbab"
+string2="aabbabbbaaaababaaab"
+
+substring="ab"
+
+print("The number of", substring,"present in ", string1," is",string1.count('ab'),"and in",string2," is",string2.count('ab'))
+while(1):
+    check=input("Do you want to check more?(yes/no)")
+
+    if check=='yes':
+        string=input("Enter the string: ")
+        sub=input('Enter the substring: ')
+
+        cou=string.count(sub)
+
+        print("The number of", sub,"present in ",string ,"is ",cou)
+    
+    elif check=='no':
+        print("It's ok")
+
+    else:
+        print("Please enter the correct input")
+    
+    
+    
+
+
+
